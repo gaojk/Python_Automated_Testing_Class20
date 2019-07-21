1a897f135ea9676143b7871b522fcb420db9cd58
yinjinbiao <abiao7722882@163.com>
2019-07-21 23:00:47 +0800

    0719作业

---
+++ Python_0719_job/__init__.py
@@ -0,0 +1,2 @@
+# @Time :2019/7/20 10:07
+# @Author :jinbiao
---
+++ Python_0719_job/test_divide.py
@@ -0,0 +1,43 @@
+# @Time :2019/7/21 13:48
+# @Author :jinbiao
+from Python_0719_job import match_count
+from Python_0719_job.operation_excel import OperationExcel
+import unittest
+from ddt import ddt, data
+
+oe = OperationExcel(excel_name="test_data.xlsx", sheet_name="divide")
+test_data = oe.get_data()
+
+
+@ddt
+class Testoperation(unittest.TestCase):   # 创建一个测试类，继承unitest包下的TestCase类
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.write_file = open("result.txt", mode="a", encoding="utf-8")
+        cls.write_file.write("{:-^50}\n".format("测试用例开始执行"))
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.write_file.write("{:-^50}\n".format("测试用例执行结果"))
+        cls.write_file.close()
+
+    @data(*test_data)
+    def test_divide(self, data):
+        # for data in test_data:
+        one_operation = match_count.Count(data["l_data"], data["r_data"])    # 创建一个运算对象
+        actual = one_operation.divide()    # 调用divide方法，返回计算结果
+        expectation = data["expect"]     # 定义期望值
+        description = data["description"]
+        try:
+            self.assertEqual(expectation, actual, msg=f"{description}{one_operation.number1}/{one_operation.number2}不等于{expectation}")  # 判断期望值是否等于实际结果
+            self.write_file.write("用例执行结果为：【PASS】\n")
+            oe.write_data(data["case_id"]+1, actual=actual, result="PASS")
+        except Exception as e:
+            self.write_file.write("用例执行结果为：【FAIL】{}\n".format(e))
+            oe.write_data(data["case_id"] + 1, actual=actual, result="FAIL")
+            raise e
+
+
+if __name__ == '__main__':
+    unittest.main()
